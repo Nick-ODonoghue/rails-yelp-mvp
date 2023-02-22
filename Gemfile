e5ce31747b463727d33d451ca71311edae2ed9aa
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-02-22 14:08:55 +0000

    created restaurants controller

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
+# FAKER
+gem 'faker'
 # Simple forms
 gem "simple_form", github: "heartcombo/simple_form"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+50.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
+# puts "Created #{restaurant.name}"
+puts "Finished!"
